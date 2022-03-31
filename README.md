7a3e0a6bacbcf241e673b8c985bc0918145ff1cd
Craig <indigomx9@gmail.com>
2022-03-31 14:31:03 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Shopping Cart
+
+Initial setup of the project.  Created with React 18, Redux-Toolkit, and Firebase Realtime Database.
+
+
+
---
+++ src/app/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis vero, nihil totam adipisci a accusamus laudantium quibusdam quisquam ipsam dolores sint ipsa tempora repellat sunt? Assumenda velit eum incidunt architecto.</p>
+        </React.Fragment>
+    );
+};
+
+
+
