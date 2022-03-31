a5dc63f5b5473e3bbafe894be69ba3fdd9cf804b
Craig <indigomx9@gmail.com>
2022-03-31 18:39:27 -0400

    Added the AuthSlice.

---
+++ src/global/AuthSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const AuthSlice = createSlice({
+    name: "auth",
+    initialState: {
+        isLoggedIn: false
+    },
+    reducers: {
+        login(state) {
+            state.isLoggedIn = true
+        },
+        logout(state) {
+            state.isLoggedIn = false
+        },
+    },
+});
+
+export const AuthActions = AuthSlice.actions;
+export const AuthReducer = AuthSlice.reducer;
+
+
--- src/global/RootReducer.js
+++ src/global/RootReducer.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { UIReducer } from "./UISlice";
+import { AuthReducer } from "./AuthSlice";
 
 export const RootReducer = configureStore({
     reducer: {
         ui: UIReducer,
+        auth: AuthReducer,
     },
 });
 
