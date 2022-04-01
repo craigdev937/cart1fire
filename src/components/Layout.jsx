4e245f5752963979ea7f0e63f763942d7e8a2016
Craig <indigomx9@gmail.com>
2022-03-31 21:19:37 -0400

    Added Products and ProdList.

---
+++ src/components/Layout.jsx
@@ -0,0 +1,3 @@
+import React from "react";
+// Header
+import {  } from "../data";
---
+++ src/components/ProdList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./ProdList.css";
+import { useDispatch } from "react-redux";
+
+export const ProdList = 
+({ name, id, imgURL, price }) => {
+    return (
+        <main className="card">
+            <img src={imgURL} alt={name} />
+            <h2>{name}</h2>
+            <p>$ {price}</p>
+            <button onClick={addToCart}>Add to cart</button>
+        </main>
+    );
+};
+
+
+
