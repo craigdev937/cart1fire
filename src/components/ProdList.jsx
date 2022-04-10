2932cee47d25446c8c6f8ea3f88421b4db037f02
Craig <indigomx9@gmail.com>
2022-04-09 22:50:41 -0400

    Updated ProdList and the Main component.

--- src/components/ProdList.jsx
+++ src/components/ProdList.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import "./ProdList.css";
 import { useDispatch } from "react-redux";
+import { CartActions } from "../global/CartSlice";
 
 export const ProdList = 
 ({ name, id, imgURL, price }) => {
+    const dispatch = useDispatch();
+    const addToCart = () => {
+        dispatch(CartActions.addToCart({
+            name, id, price
+        }))
+    };
+
     return (
         <main className="card">
             <img src={imgURL} alt={name} />
             <h2>{name}</h2>
             <p>$ {price}</p>
-            {/* <button onClick={addToCart}>Add to cart</button> */}
+            <button 
+                onClick={addToCart}
+                >Add to cart
+            </button>
         </main>
     );
 };
--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -1,23 +1,41 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Auth } from "./Auth";
 import { Layout } from "../components/Layout";
+import { Notification } from "../components/Notification";
+import { fetchData, sendCartData } from "../global/FetchAPI";
 
-const URL = "https://redux-toolkit-http-c01a8-default-rtdb.firebaseio.com/cartItems.json";
 export const Main = () => {
+    let isFirstRender = true;
+    const dispatch = useDispatch();
+    const notification = useSelector(
+        (state) => state.ui.notification);
     const cart = useSelector((state) => state.cart);
-    const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);    
-    
+    const isLoggedIn = useSelector(
+        (state) => state.auth.isLoggedIn);
+
     React.useEffect(() => {
-        fetch(URL, {
-            method: "PUT",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(cart),
-        })
-    }, [cart]);
+        dispatch(fetchData());
+    }, [dispatch]);
+
+    React.useEffect(() => {
+        if (isFirstRender) {
+            isFirstRender = false;
+            return;
+        };
+        if (cart.changed) {
+            dispatch(sendCartData(cart));
+        };
+    }, [cart, dispatch]);
 
     return (
         <React.Fragment>
+            {notification && (
+                <Notification 
+                    type={notification.type} 
+                    message={notification.message}
+                />
+            )}
             {!isLoggedIn && <Auth />}
             {isLoggedIn && <Layout />}
         </React.Fragment>
